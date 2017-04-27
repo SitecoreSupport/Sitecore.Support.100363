ddb54bd833983baaad80b6e9ef8a987819cc9761
ido <ido@WS-IDO1UA1>
2017-04-27 15:47:13 +0300

    First commit - patch is ready

---
+++ src/Sitecore.Support.100363/Pipelines/Save/DecodeDoubleQuoteInLink.cs
@@ -0,0 +1,42 @@
+﻿using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Pipelines.Save;
+
+namespace Sitecore.Support.Pipelines.Save
+{
+  public class DecodeDoubleQuoteInLink
+  {
+    public void Process(SaveArgs args)
+    {
+      if (Context.Site != null && Context.PageMode.IsExperienceEditor && Client.ContentDatabase != null)
+      {
+        Assert.ArgumentNotNull(args, "args");
+        Assert.IsNotNull(args.Items, "args.Items");
+
+        SaveArgs.SaveItem[] items = args.Items;
+
+        foreach (var item in items)
+        {
+          Item item2 = Client.ContentDatabase.Items[item.ID, item.Language, item.Version];
+
+          if (null != item2)
+          {
+            foreach (var field in item.Fields)
+            {
+              Field field2 = item2.Fields[field.ID];
+              if (((field2 != null) && !field2.IsBlobField))
+              {
+                string str = field.Value;
+                if (!string.IsNullOrEmpty(str))
+                {
+                  field.Value = str.Replace("-,sc_dq,-", "&quot;");
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.100363/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.100363")]
+[assembly: AssemblyProduct("Sitecore.Support.100363")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Double quote in a link description prevents a page from saving in the Experience Editor")]
+
